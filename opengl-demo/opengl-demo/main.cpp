894a7819af5891b8e9c31829b68e75988a9a52b5
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-03-04 23:55:55 +0100

    Implemented drawing basic triangle

--- opengl-demo/opengl-demo/main.cpp
+++ opengl-demo/opengl-demo/main.cpp
@@ -29,6 +29,35 @@ int initializeGLAD()
 }
 //
 
+// TODO: Move this method somewhere else (but probably will be deleted in the meantime)
+uint initializeTriangle()
+{
+    float vertices[] =
+    {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    // Copy and bind vertices to opengl buffer
+    uint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Set vertex attributes
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    return VAO;
+}
+
 int main()
 {
     GLFWwindow* window;
@@ -44,8 +73,20 @@ int main()
     if (!(shaderProgram = applyShaders()))
         return EXIT_FAILURE;
 
+    // Get VAO corresponding to default triangle
+    uint VAO = initializeTriangle();
+
     while (!glfwWindowShouldClose(window))
     {
+        // Clear the scene
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw object referenced by VAO
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Double buffering
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
