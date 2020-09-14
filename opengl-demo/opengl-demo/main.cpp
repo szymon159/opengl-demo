7f7663fcef5bf8ba049b89f4564ef3051175a75f
Szymon Stasiak <szymon-stasiak@outlook.com>
2020-09-14 03:44:43 +0200

    Update background color

--- opengl-demo/opengl-demo/main.cpp
+++ opengl-demo/opengl-demo/main.cpp
@@ -151,7 +151,7 @@ int main()
         previousTime = time;
 
         // Clear the scene
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClearColor(0.5f, 0.7f, 0.95f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Update models
